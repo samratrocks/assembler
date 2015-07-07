e6c25f32b2a04a14010026f610b7f6e12bc26718
samash <samrat.shiwakoti@gmail.com>
2015-07-07 11:50:25 -0400

    Add basic functionality to Parser.

--- parser.cpp
+++ parser.cpp
@@ -1,1 +1,20 @@
+#include <parser.h>
+#include <iostream>
+#include <fstream>
 
+using std::ios;
+using std::ifstream;
+using std::cerr;
+using std::endl;
+
+int main()
+{
+    // Open the file
+    ifstream inputFile("file.hack", ios::in);
+
+    // exit if the file couldn't be found
+    if (!inputFile) {
+        cerr << "File not found..." << endl;
+        exit(1);
+    }
+}
