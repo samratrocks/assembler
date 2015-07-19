b3ba1a644687a66874eb36a7ca2fa49db5c6c48e
samash <samrat.shiwakoti@gmail.com>
2015-07-19 01:12:41 -0400

    Add readAndParse method to Parser module.

--- parser.c
+++ parser.c
@@ -4,16 +4,18 @@
 /* function prototypes */
 void init();
 int getNumberOfLines();
+void readAndParse();
 
 
 /* global variables */
 FILE *fin;				/* handle to the input file */
-int currentLine				/* keeps track of the line being parsed */
-int numberOfLines			/* keeps track of the number of lines the program contains */
+int currentLine;				/* keeps track of the line being parsed */
+int numberOfLines;			/* keeps track of the number of lines the program contains */
 
 int main(void)
 {
   init(); 
+  readAndParse();
 }
 
 void init()
@@ -24,13 +26,27 @@ void init()
   }
 }
 
+void readAndParse()
+{
+  /* This function depends upon parse() to parse the data
+   * that it reads.
+   */
+  char * line = NULL;
+  ssize_t read;
+  size_t len = 0;
+  while ((read = getline(&line, &len, fin)) != -1) {
+    printf("%s", line);
+    /* All the parsing code goes here!" */
+  }
+}
+
 void advanceProgram()
 {
   /* Performs the same function as an INC command in a assemlby program.
      In this module this is acheived by increasign the value of the program
      line counter.
   */
-  currentLine += 1;
+  //currentLine += 1;
 }
 
 int getNumberOfLines()
@@ -38,5 +54,5 @@ int getNumberOfLines()
   /* Count the total number of lines. This can be done by counting the number
      of newlines ('\n') in the program.
   */
- 
+  return 10;
 }
